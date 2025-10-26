ab5ab25c92f09c73d8e86561e974e9a8f3c94719
krishnasabbu <krishna.sabbu@gmail.com>
2025-10-26 18:47:27 +0530

    Updated championChallengeService.ts

--- src/services/championChallengeService.ts
+++ src/services/championChallengeService.ts
@@ -2,6 +2,14 @@ class ChampionChallengeService {
   async executeComparison(data: any): Promise<any> {
     return {};
   }
+
+  async listExecutions(): Promise<any[]> {
+    return [];
+  }
+
+  async loadExecution(executionId: string): Promise<any | null> {
+    return null;
+  }
 }
 
 export const championChallengeService = new ChampionChallengeService();
